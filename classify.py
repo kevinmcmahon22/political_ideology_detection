cce38897e585110a5e44ef1359a1e2a0bf360621
kevinmcmahon22 <46839498+kevinmcmahon22@users.noreply.github.com>
2021-11-11 22:38:29 -0500

    get LR running locally

--- classify.py
+++ classify.py
@@ -1,24 +1,35 @@
 import os
 import pandas as pd
+from sklearn import svm
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 
+df_train = pd.read_csv('df_convote_train.csv')
+df_test = pd.read_csv('df_convote_test.csv')
 
+X_train = df_train['text']
+y_train = df_train['political_party']
+X_test = df_test['text']
+y_test = df_test['political_party']
+# X_train, X_validation, y_train, y_validation = train_test_split(X_train, y_train, test_size=0.2, random_state = 42)
 
-convote_data['text'] = convote_data['text'].apply(clean_text)
-X = convote_data.text
-y = convote_data.party_label
 tf_idf = TfidfVectorizer(stop_words = 'english')
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state = 42)
-X_train_fit = tf_idf.fit_transform(X_train)
-X_test_fit = tf_idf.transform(X_test)
+X_train = tf_idf.fit_transform(X_train)
+X_test = tf_idf.transform(X_test)
+
+# Logistic Regression
 model = LogisticRegression()
-model.fit(X_train_fit,y_train)
-y_predict = model.predict(X_test_fit)
-Y_predict_train = model.predict(X_train_fit)
-score_train = accuracy_score(y_train, Y_predict_train)
-score_test = accuracy_score(y_test, y_predict)
-score_train
-score_test
+model.fit(X_train,y_train)
+y_predict = model.predict(X_test)
+test_accuracy = accuracy_score(y_test, y_predict)
+print(f'LR: {round(test_accuracy*100,5)}%')
+
+# Support Vector Regression - doesn't work
+# regr = svm.SVR()
+# regr.fit(X_train, y_train)
+# regr.predict(X_test)
+# y_predict = regr.predict(X_test)
+# test_accuracy = accuracy_score(y_test, y_predict)
+# print(f'SVR: {round(test_accuracy*100,5)}%')
--- data.py
+++ data.py
@@ -25,23 +25,30 @@ def create_df_convote(filenames):
         filename = file_path.split('\\')
         filename_tokens = filename[-1].split('_')
         bill_debate_number = filename_tokens[0]
+
         party = filename_tokens[-1][0] # D or R
+        if party == 'R':
+            political_party = 1
+        else:
+            political_party = 0
+
         m = filename_tokens[-1][1]
         if m == 'M':
             mentioned_bill = 1
         else:
             mentioned_bill = 0
+
         vote = filename_tokens[-1][2]
         if vote == 'Y':
             voted_yes = 1
         else:
             voted_yes = 0
         # add to list
-        items.append([text, bill_debate_number, party, mentioned_bill, voted_yes])
+        items.append([text, bill_debate_number, political_party, mentioned_bill, voted_yes])
     
     # convert list to df
     new_df = pd.DataFrame(items)
-    return new_df.rename(index=str, columns={0: 'text', 1: 'bill_debate_number', 2: 'party', 3: 'mentioned_bill', 4: 'voted_yes'})
+    return new_df.rename(index=str, columns={0: 'text', 1: 'bill_debate_number', 2: 'political_party', 3: 'mentioned_bill', 4: 'voted_yes'})
 
 # main
 filenames_train = glob.glob('data/convote_v1.1/data_stage_one/training_set/*.txt')
@@ -50,5 +57,5 @@ filenames_test = glob.glob('data/convote_v1.1/data_stage_one/test_set/*.txt')
 df_train = create_df_convote(filenames_train)
 df_test = create_df_convote(filenames_test)
 
-df_train.to_csv('df_convote_train')
-df_test.to_csv('df_convote_test')
+df_train.to_csv('df_convote_train.csv')
+df_test.to_csv('df_convote_test.csv')
